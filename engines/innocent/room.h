330d7a737200940b9bab91f4540f3ae0dd3845b3
Rafał Rzepecki <divided.mind@gmail.com>
2009-08-06 17:44:18 +0200

    Fix compilation for new scummvm interfaces.

--- engines/innocent/room.h
+++ engines/innocent/room.h
@@ -53,7 +53,7 @@ public:
 		Common::Rect _rect;
 	};
 
-	const Common::List<Exit *> &exits() const { return _exits; }
+	Common::List<Exit *> exits() const { return _exits; }
 
 	void addActorFrame(Common::Point p, std::vector<byte> nexts);
 	Actor::Frame getFrame(uint16 index) const {
