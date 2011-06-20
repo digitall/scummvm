30b5634a9f431ac389b15af149dead8cc06cc748
Engin Manap <enginmanap@gmail.com>
2011-06-21 00:23:54 +0300

    CRUISE: move resetBackgroundIncrustList function to BackgroundIncrustListNode

--- engines/cruise/saveload.cpp
+++ engines/cruise/saveload.cpp
@@ -730,7 +730,7 @@ void initVars() {
 	resetPtr(&cellHead);
 
 	resetActorPtr(&actorHead);
-	resetBackgroundIncrustList(&backgroundIncrustListHead);
+	backgroundIncrustListHead.resetBackgroundIncrustList();
 
 	vblLimit = 0;
 	remdo = 0;
