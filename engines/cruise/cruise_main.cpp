1e2604eea898fa43bc1c91deb8a452e810ddf313
Engin Manap <enginmanap@gmail.com>
2011-07-05 11:24:57 +0300

    CRUISE: Move removeCell function to CellListNode as method.

--- engines/cruise/cruise_main.cpp
+++ engines/cruise/cruise_main.cpp
@@ -1966,7 +1966,7 @@ void CruiseEngine::mainLoop() {
 					userEnabled = false;
 				}
 			} else if (autoMsg != -1) {
-				removeCell(&cellHead, autoOvl, autoMsg, 5, masterScreen);
+				cellHead.removeCell(autoOvl, autoMsg, 5, masterScreen);
 				autoMsg = -1;
 			}
 		} else {
