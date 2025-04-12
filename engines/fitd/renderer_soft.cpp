ea14a2bc6c626d3b2b9d64eb82868fedb33b52bc
scemino <scemino74@gmail.com>
2025-04-12 10:38:10 +0200

    FITD: Fix some holes in software renderer + bad mask

--- engines/fitd/renderer_soft.cpp
+++ engines/fitd/renderer_soft.cpp
@@ -34,6 +34,8 @@ enum {
 	NUM_MAX_FLAT_VERTICES = 5000 * 3,
 	MAX_POINTS_PER_POLY = 50,
 	NUM_MAX_MASKS = 32,
+	WIDTH = 320,
+	HEIGHT = 200,
 };
 
 struct polyVertex {
@@ -49,7 +51,7 @@ struct Mask {
 	int16 maskY1;
 	int16 maskX2;
 	int16 maskY2;
-	byte mask[320 * 200];
+	byte mask[WIDTH * HEIGHT];
 };
 
 struct ClipMask {
@@ -60,15 +62,15 @@ struct ClipMask {
 };
 
 struct State {
-	int16 tabVerticXmin[200];
-	int16 tabVerticXmax[200];
-	float tabVerticZmin[200]; // TODO: improve his, this not necessary to store all these data
-	float tabVerticZmax[200];
+	int16 tabVerticXmin[HEIGHT];
+	int16 tabVerticXmax[HEIGHT];
+	float tabVerticZmin[HEIGHT]; // TODO: improve his, this not necessary to store all these data
+	float tabVerticZmax[HEIGHT];
 	polyVertex flatVertices[NUM_MAX_FLAT_VERTICES];
 	polyVertex tmpVertices[NUM_MAX_FLAT_VERTICES];
-	byte physicalScreen[320 * 200];
-	byte physicalScreenRGB[320 * 200 * 3];
-	float zBuffer[320 * 200];
+	byte physicalScreen[WIDTH * HEIGHT];
+	byte physicalScreenRGB[WIDTH * HEIGHT * 3];
+	float zBuffer[WIDTH * HEIGHT];
 	byte RGB_Pal[256 * 3];
 
 	int16 polyMinX;
@@ -124,8 +126,8 @@ Renderer createSoftwareRenderer() {
 static void renderer_init() {
 	_state = (State *)malloc(sizeof(State));
 	_state->numMasks = 0;
-	initGraphics(320, 200);
-	g_engine->_screen = new Graphics::Screen(320, 200);
+	initGraphics(WIDTH, HEIGHT);
+	g_engine->_screen = new Graphics::Screen(WIDTH, HEIGHT);
 }
 
 static void renderer_deinit() {
@@ -137,7 +139,7 @@ static void renderer_deinit() {
 static void renderer_startFrame() {
 	g_engine->_screen->clear();
 	g_engine->_screen->clearDirtyRects();
-	for (int i = 0; i < 320 * 200; i++) {
+	for (int i = 0; i < WIDTH * HEIGHT; i++) {
 		_state->zBuffer[i] = __FLT_MAX__;
 	}
 }
@@ -145,7 +147,7 @@ static void renderer_startFrame() {
 static void renderer_drawBackground() {
 	g_engine->_screen->setPalette(_state->RGB_Pal);
 	byte *screen = (byte *)g_engine->_screen->getBasePtr(0, 0);
-	memcpy(screen, _state->physicalScreen, 320 * 200);
+	memcpy(screen, _state->physicalScreen, WIDTH * HEIGHT);
 	g_engine->_screen->markAllDirty();
 }
 
@@ -156,7 +158,7 @@ static void renderer_setPalette(const byte *palette) {
 
 static void renderer_copyBlockPhys(byte *videoBuffer, int left, int top, int right, int bottom) {
 	byte *out = _state->physicalScreenRGB;
-	const byte *in = (const byte *)&videoBuffer[0] + left + top * 320;
+	const byte *in = (const byte *)&videoBuffer[0] + left + top * WIDTH;
 
 	while ((right - left) % 4) {
 		right++;
@@ -167,8 +169,8 @@ static void renderer_copyBlockPhys(byte *videoBuffer, int left, int top, int rig
 	}
 
 	for (int i = top; i < bottom; i++) {
-		in = (const byte *)&videoBuffer[0] + left + i * 320;
-		byte *out2 = _state->physicalScreen + left + i * 320;
+		in = (const byte *)&videoBuffer[0] + left + i * WIDTH;
+		byte *out2 = _state->physicalScreen + left + i * WIDTH;
 		for (int j = left; j < right; j++) {
 			byte color = *(in++);
 
@@ -185,7 +187,7 @@ static void renderer_refreshFrontTextureBuffer() {
 	byte *out = _state->physicalScreenRGB;
 	const byte *in = _state->physicalScreen;
 
-	for (int i = 0; i < 200 * 320; i++) {
+	for (int i = 0; i < HEIGHT * WIDTH; i++) {
 		byte color = *(in++);
 		*(out++) = _state->RGB_Pal[color * 3];
 		*(out++) = _state->RGB_Pal[color * 3 + 1];
@@ -218,28 +220,28 @@ static void renderer_createMask(const uint8 *mask, int roomId, int maskId, byte 
 	pMask->maskY1 = maskY1;
 	pMask->maskX2 = maskX2;
 	pMask->maskY2 = maskY2;
-	memcpy(pMask->mask, mask, sizeof(byte) * 320 * 200);
+	memcpy(pMask->mask, mask, sizeof(byte) * WIDTH * HEIGHT);
 }
 
 static void renderer_setClip(float left, float top, float right, float bottom) {
 
 	_state->clipMask.x = left;
 	_state->clipMask.y = top;
-	_state->clipMask.w = right - left;
-	_state->clipMask.h = bottom - top;
+	_state->clipMask.w = right - left + 1;
+	_state->clipMask.h = bottom - top + 1;
 
-	_state->clipMask.x = CLIP(_state->clipMask.x, (int16)0, (int16)320);
-	_state->clipMask.y = CLIP(_state->clipMask.y, (int16)0, (int16)200);
+	_state->clipMask.x = CLIP(_state->clipMask.x, (int16)0, (int16)WIDTH);
+	_state->clipMask.y = CLIP(_state->clipMask.y, (int16)0, (int16)HEIGHT);
 
-	_state->clipMask.w = CLIP(_state->clipMask.w, (int16)0, (int16)(320 - _state->clipMask.x));
-	_state->clipMask.h = CLIP(_state->clipMask.h, (int16)0, (int16)(200 - _state->clipMask.y));
+	_state->clipMask.w = CLIP(_state->clipMask.w, (int16)0, (int16)(WIDTH - _state->clipMask.x));
+	_state->clipMask.h = CLIP(_state->clipMask.h, (int16)0, (int16)(HEIGHT - _state->clipMask.y));
 }
 
 static void renderer_clearClip() {
 	_state->clipMask.x = 0;
 	_state->clipMask.y = 0;
-	_state->clipMask.x = 320;
-	_state->clipMask.w = 200;
+	_state->clipMask.x = WIDTH;
+	_state->clipMask.w = HEIGHT;
 }
 
 static void renderer_drawMask(int roomId, int maskId) {
@@ -247,8 +249,8 @@ static void renderer_drawMask(int roomId, int maskId) {
 		Mask *pMask = &_state->masks[i];
 		if (pMask->roomId == roomId && pMask->maskId == maskId) {
 			byte *s = (byte *)g_engine->_screen->getBasePtr(_state->clipMask.x, _state->clipMask.y);
-			byte *p = &_state->physicalScreen[_state->clipMask.y * 320 + _state->clipMask.x];
-			byte *m = &pMask->mask[_state->clipMask.y * 320 + _state->clipMask.x];
+			byte *p = &_state->physicalScreen[_state->clipMask.y * WIDTH + _state->clipMask.x];
+			byte *m = &pMask->mask[_state->clipMask.y * WIDTH + _state->clipMask.x];
 			for (size_t h = 0; h < _state->clipMask.h; h++) {
 				for (size_t w = 0; w < _state->clipMask.w; w++) {
 					if (*m) {
@@ -258,9 +260,9 @@ static void renderer_drawMask(int roomId, int maskId) {
 					p++;
 					s++;
 				}
-				m += 320 - _state->clipMask.w;
-				p += 320 - _state->clipMask.w;
-				s += 320 - _state->clipMask.w;
+				m += WIDTH - _state->clipMask.w;
+				p += WIDTH - _state->clipMask.w;
+				s += WIDTH - _state->clipMask.w;
 			}
 			return;
 		}
@@ -342,9 +344,9 @@ static int16 rightClip(polyVertex **polys, int16 num) {
 		// 0x01 : point 0 clipped
 		// 0x02 : point 1 clipped
 		// 0x03 : both clipped
-		uint8 clipFlag = (p1->X > 319) ? 2 : 0;
+		uint8 clipFlag = (p1->X >= WIDTH) ? 2 : 0;
 
-		if (p0->X > 319) {
+		if (p0->X >= WIDTH) {
 			if (clipFlag) {
 				continue; // both clipped, skip point 0
 			}
@@ -365,9 +367,9 @@ static int16 rightClip(polyVertex **polys, int16 num) {
 			const int32 dx = p1->X - p0->X;
 			const int32 dy = p1->Y - p0->Y;
 			const int32 dz = p1->Z - p0->Z;
-			const int32 dxClip = 319 - p0->X;
+			const int32 dxClip = (WIDTH - 1) - p0->X;
 
-			pTabPolyClip->X = (int16)319;
+			pTabPolyClip->X = (int16)(WIDTH - 1);
 			pTabPolyClip->Y = (int16)(p0->Y + ((dxClip * dy) / dx));
 			pTabPolyClip->Z = (p0->Z + ((float)(dxClip * dz) / dx));
 
@@ -456,9 +458,9 @@ static int16 bottomClip(polyVertex **polys, int16 num) {
 		// 0x01 : point 0 clipped
 		// 0x02 : point 1 clipped
 		// 0x03 : both clipped
-		uint8 clipFlag = (p1->Y > 199) ? 2 : 0;
+		uint8 clipFlag = (p1->Y >= HEIGHT) ? 2 : 0;
 
-		if (p0->Y > 199) {
+		if (p0->Y >= HEIGHT) {
 			if (clipFlag) {
 				continue; // both clipped, skip point 0
 			}
@@ -479,10 +481,10 @@ static int16 bottomClip(polyVertex **polys, int16 num) {
 			const int32 dx = p1->X - p0->X;
 			const int32 dy = p1->Y - p0->Y;
 			const int32 dz = p1->Z - p0->Z;
-			const int32 dyClip = 199 - p0->Y;
+			const int32 dyClip = (HEIGHT - 1) - p0->Y;
 
 			pTabPolyClip->X = (int16)(p0->X + ((dyClip * dx) / dy));
-			pTabPolyClip->Y = (int16)199;
+			pTabPolyClip->Y = (int16)(HEIGHT - 1);
 			pTabPolyClip->Z = (p0->Z + ((float)(dyClip * dz) / dy));
 
 			++pTabPolyClip;
@@ -506,12 +508,12 @@ static int16 poly_clip(polyVertex **polys, int16 num) {
 		_state->polyMinX = 0;
 		hasBeenClipped = true;
 	}
-	if (_state->polyMaxX > 319) {
+	if (_state->polyMaxX >= WIDTH) {
 		clippedNum = rightClip(polys, clippedNum);
 		if (!clippedNum) {
 			return 0;
 		}
-		_state->polyMaxX = 319;
+		_state->polyMaxX = (WIDTH - 1);
 		hasBeenClipped = true;
 	}
 	if (_state->polyMinY < 0) {
@@ -522,12 +524,12 @@ static int16 poly_clip(polyVertex **polys, int16 num) {
 		_state->polyMinY = 0;
 		hasBeenClipped = true;
 	}
-	if (_state->polyMaxY > 199) {
+	if (_state->polyMaxY >= HEIGHT) {
 		clippedNum = bottomClip(polys, clippedNum);
 		if (!clippedNum) {
 			return 0;
 		}
-		_state->polyMaxY = 199;
+		_state->polyMaxY = (HEIGHT - 1);
 		hasBeenClipped = true;
 	}
 
@@ -542,22 +544,18 @@ static int16 poly_clip(polyVertex **polys, int16 num) {
 
 static void poly_setMinMax(polyVertex *pTabPoly, int16 num) {
 	int32 incY = -1;
-	int32 dx, dy, x, y;
-	int32 step, reminder;
-	const polyVertex *p0;
-	const polyVertex *p1;
-	int16 *pVertic;
 	float *pZ;
 	for (int i = 0; i < num; i++, pTabPoly++) {
-		p0 = pTabPoly;
-		p1 = p0 + 1;
+		const polyVertex *p0 = pTabPoly;
+		const polyVertex *p1 = p0 + 1;
+		int16 *pVertic;
 
-		dy = p1->Y - p0->Y;
+		int32 dy = p1->Y - p0->Y;
 		if (dy == 0) {
 			// forget same Y points
 			continue;
 		} else if (dy > 0) {
-			// Y therefore goes down left buffer
+			// Y therefore goes down x max buffer
 			if (p0->X <= p1->X) {
 				incY = 1;
 			} else {
@@ -583,21 +581,21 @@ static void poly_setMinMax(polyVertex *pTabPoly, int16 num) {
 			pZ = &_state->tabVerticZmin[p0->Y];
 		}
 
-		dx = (p1->X - p0->X) << 16;
-		step = dx / dy;
-		reminder = ((dx % dy) >> 1) + 0x7FFF;
+		int32 dx = (p1->X - p0->X) << 16;
+		int32 step = dx / dy;
+		int32 reminder = ((dx % dy) >> 1) + 0x7FFF;
 		float dz = (float)(p1->Z - p0->Z) / dy;
 
 		dx = step >> 16; // recovery part high division (entire)
 		step &= 0xFFFF;  // preserves lower part (mantissa)
-		x = p0->X;
+		int32 x = p0->X;
 		float z = p0->Z;
 
-		for (y = dy; y >= 0; --y) {
+		for (int32 y = 0; y <= dy; y++) {
 			*pVertic = (int16)x;
 			*pZ = z;
 			assert(x >= 0);
-			assert(x < 320);
+			assert(x < WIDTH);
 			assert(z >= 0);
 			assert(z < 32000);
 			pVertic += incY;
@@ -668,10 +666,11 @@ static int16 prepare_poly(const int16 *buffer, polyVertex *pTabPoly, int16 num) 
 	set_boundingBox(pTabPoly, num);
 
 	// no vertices
-	if (_state->polyMinY > _state->polyMaxY || _state->polyMaxX < 0 || _state->polyMinX > 319 || _state->polyMaxY < 0 || _state->polyMinY > 199) {
+	if (_state->polyMinY > _state->polyMaxY || _state->polyMaxX < 0 || _state->polyMinX >= WIDTH || _state->polyMaxY < 0 || _state->polyMinY >= HEIGHT) {
 		return 0;
 	}
 
+	// copy the first point to the end of the polygon
 	pTabPoly[num] = pTabPoly[0];
 	polyVertex *polys[] = {pTabPoly, _state->tmpVertices};
 
@@ -679,9 +678,7 @@ static int16 prepare_poly(const int16 *buffer, polyVertex *pTabPoly, int16 num) 
 	if (!num)
 		return 0;
 
-	pVertex = polys[0];
-
-	poly_setMinMax(pVertex, num);
+	poly_setMinMax(polys[0], num);
 	return num;
 }
 
@@ -698,16 +695,16 @@ static void renderer_fillPoly(const int16 *buffer, int numPoint, byte color, uin
 		return;
 
 	assert(_state->polyMinY >= 0);
-	assert(_state->polyMaxY < 200);
+	assert(_state->polyMaxY < HEIGHT);
 	assert(_state->polyMinX >= 0);
-	assert(_state->polyMaxX < 320);
+	assert(_state->polyMaxX < WIDTH);
 	int16 y = _state->polyMinY;
 	byte *pDestLine = (uint8 *)g_engine->_screen->getBasePtr(0, y);
 	const int16 *pVerticXmin = &_state->tabVerticXmin[y];
 	const int16 *pVerticXmax = &_state->tabVerticXmax[y];
 	const float *pVerticZmin = &_state->tabVerticZmin[y];
 	const float *pVerticZmax = &_state->tabVerticZmax[y];
-	float *zBuffer = &_state->zBuffer[y * 320];
+	float *zBuffer = &_state->zBuffer[y * WIDTH];
 
 	switch (polyType) {
 	case 0: // flat (triste)
@@ -727,14 +724,15 @@ static void renderer_fillPoly(const int16 *buffer, int numPoint, byte color, uin
 			float z = zMin;
 			for (int16 x = xMin; x <= xMax; x++) {
 				if (z < zBuffer[x]) {
-					*pDest++ = (byte)color;
+					*pDest = (byte)color;
 					zBuffer[x] = z;
 				}
+				pDest++;
 				z += dz;
 			}
 
-			pDestLine += 320;
-			zBuffer += 320;
+			pDestLine += WIDTH;
+			zBuffer += WIDTH;
 			if (Debug && xMin <= xMax) {
 				g_engine->_screen->addDirtyRect(Common::Rect(Common::Point(xMin, y), xMax - xMin + 1, 1));
 				renderer_updateScreen();
@@ -767,14 +765,15 @@ static void renderer_fillPoly(const int16 *buffer, int numPoint, byte color, uin
 				col = ((col + acc) & 0xFF03) + (uint16)color;
 				acc = ROL16(acc, 2) + 1;
 				if (z < zBuffer[x]) {
-					*pDest++ = (byte)col;
+					*pDest = (byte)col;
 					zBuffer[x] = z;
 				}
+				pDest++;
 				z += dz;
 			}
 
-			pDestLine += 320;
-			zBuffer += 320;
+			pDestLine += WIDTH;
+			zBuffer += WIDTH;
 			if (Debug && xMin <= xMax) {
 				g_engine->_screen->addDirtyRect(Common::Rect(Common::Point(xMin, y), xMax - xMin + 1, 1));
 				renderer_updateScreen();
@@ -805,14 +804,14 @@ static void renderer_fillPoly(const int16 *buffer, int numPoint, byte color, uin
 			for (int16 x = xMin; x <= xMax; x++) {
 				if (z < zBuffer[x]) {
 					*pDest = (byte)color | (*pDest & 0x0F);
-					pDest++;
 					zBuffer[x] = z;
 				}
+				pDest++;
 				z += dz;
 			}
 
-			pDestLine += 320;
-			zBuffer += 320;
+			pDestLine += WIDTH;
+			zBuffer += WIDTH;
 			if (Debug && xMin <= xMax) {
 				g_engine->_screen->addDirtyRect(Common::Rect(Common::Point(xMin, y), xMax - xMin + 1, 1));
 				renderer_updateScreen();
@@ -841,9 +840,10 @@ static void renderer_fillPoly(const int16 *buffer, int numPoint, byte color, uin
 			float z = zMin;
 			for (int16 x = xMin; x <= xMax; x++) {
 				if (z < zBuffer[x]) {
-					*pDest++ = (byte)color;
+					*pDest = (byte)color;
 					zBuffer[x] = z;
 				}
+				pDest++;
 				z += dz;
 			}
 
@@ -855,8 +855,8 @@ static void renderer_fillPoly(const int16 *buffer, int numPoint, byte color, uin
 				}
 			}
 
-			pDestLine += 320;
-			zBuffer += 320;
+			pDestLine += WIDTH;
+			zBuffer += WIDTH;
 			if (Debug && xMin <= xMax) {
 				g_engine->_screen->addDirtyRect(Common::Rect(Common::Point(xMin, y), xMax - xMin + 1, 1));
 				renderer_updateScreen();
@@ -905,8 +905,8 @@ static void renderer_fillPoly(const int16 *buffer, int numPoint, byte color, uin
 				}
 			}
 
-			pDestLine += 320;
-			zBuffer += 320;
+			pDestLine += WIDTH;
+			zBuffer += WIDTH;
 			if (Debug && xMin <= xMax) {
 				g_engine->_screen->addDirtyRect(Common::Rect(Common::Point(xMin, y), xMax - xMin + 1, 1));
 				renderer_updateScreen();
@@ -938,11 +938,11 @@ Graphics::Surface *renderer_capture() {
 #else
 	Graphics::PixelFormat format = Graphics::PixelFormat(4, 8, 8, 8, 8, 0, 8, 16, 24);
 #endif
-	s->create(320, 200, format);
+	s->create(WIDTH, HEIGHT, format);
 	byte *src = _state->physicalScreen;
 	byte *dst = (byte *)s->getPixels();
 	byte *pal = _state->RGB_Pal;
-	for (int i = 0; i < 320 * 200; i++) {
+	for (int i = 0; i < WIDTH * HEIGHT; i++) {
 		dst[0] = pal[*src * 3];
 		dst[1] = pal[*src * 3 + 1];
 		dst[2] = pal[*src * 3 + 2];
